50854df665ea7bd65a85e35991b12b1152342fd5
jkky-98 <aal2525@ajou.ac.kr>
2025-12-20 21:30:54 +0900

    feat: 프로젝트 초기 설정 및 SpringBoot 의존성 구성

---
+++ src/main/java/com/junki/demo/Spubgv2Application.java
@@ -0,0 +1,15 @@
+package com.junki.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@SpringBootApplication
+@EnableScheduling
+public class Spubgv2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spubgv2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/junki/demo/Spubgv2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.junki.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spubgv2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
